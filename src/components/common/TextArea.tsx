15b74538a28cf42a7838f0318c5f8d8c06ea60b6
hyeonji <55472485+hyeonzii@users.noreply.github.com>
2023-12-06 04:10:39 +0900

    [개발] TextArea 컴포넌트 추가 (#2)
    - 컴포넌트에 TextArea 추가

---
+++ src/components/common/TextArea.tsx
@@ -0,0 +1,44 @@
+import { TextareaHTMLAttributes } from 'react';
+import styled from 'styled-components';
+
+interface TextAreaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  width: string;
+  height: string;
+  padding: string;
+}
+
+const TextArea: React.FC<TextAreaProps> = ({ width, height, padding, placeholder, maxLength }) => {
+  return (
+    <StyledTextArea
+      width={width}
+      height={height}
+      padding={padding}
+      placeholder={placeholder}
+      maxLength={maxLength}
+    ></StyledTextArea>
+  );
+};
+
+export default TextArea;
+
+const StyledTextArea = styled.textarea<TextAreaProps>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+  padding: ${(props) => props.padding};
+  background-color: ${(props) => props.theme.color.grey90};
+  color: ${(props) => props.theme.color.white};
+  border-radius: 8px;
+  border: 0;
+  resize: none;
+  font-family: inherit;
+  font-size: inherit;
+
+  &::placeholder {
+    font-weight: 400;
+    font-size: 14px;
+    color: ${(props) => props.theme.color.grey50};
+  }
+  &:focus {
+    outline: 1px solid ${(props) => props.theme.color.blue};
+  }
+`;
