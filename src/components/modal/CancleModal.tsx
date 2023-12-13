00d4209bac1fa3db78cad90db765c903681b396c
hyeonji <55472485+hyeonzii@users.noreply.github.com>
2023-12-13 12:28:51 +0900

    [개발] 취소 모달창 수정 (#2)
    - 모달창 혼종으로 구현한거..좀 수정(?)

--- src/components/modal/CancleModal.tsx
+++ src/components/modal/CancleModal.tsx
@@ -7,6 +7,7 @@ import CommonButton, { ButtonType } from '../common/Button';
 import { InputStyle } from '../common/Input';
 import SelectDropDown from '../../components/common/SelectDropDown';
 import TextArea from '../../components/common/TextArea';
+import TextBox from '../common/TextBox';
 
 export interface CouponModalProps {
   modalOpen: boolean;
@@ -18,10 +19,6 @@ const CouponModal: React.FC<CouponModalProps> = ({ modalOpen, modalClose, onCanc
   const [reason, setReason] = useState('');
   const [reasonTitle, setReasonTitle] = useState('');
   const reasonRef = useRef<HTMLTextAreaElement>(null);
-
-  const handleInputChange = (event) => {
-    setReason(event.target.value);
-  };
 
   const handleSubmit = () => {
     if (reasonRef.current) {
@@ -44,40 +41,44 @@ const CouponModal: React.FC<CouponModalProps> = ({ modalOpen, modalClose, onCanc
   return (
     <>
       <Modal type={ModalType.POPUP} isModalOpen={modalOpen} onClose={modalClose} title="취소 요청">
-        <Spacer height={38} />
-        <InputItemContainer>
-          <Text $fontType="H3" color="white">
-            취소 사유
-          </Text>
+        <form onSubmit={handleSubmit}>
+          <Spacer height={38} />
+          <InputItemContainer>
+            <Text $fontType="H3" color="white">
+              취소 사유
+            </Text>
+            <Spacer height={10} />
+            <SelectDropDown
+              menuItems={['단순변심', '배송지연', '주문실수', '서비스 불만족']}
+              setTitle={setReasonTitle}
+              selectTitle="취소 사유 선택"
+            />
+            <Spacer height={12} />
+            <Text $fontType="H3" color="white">
+              상세 사유 &#40;선택&#41;
+            </Text>
+            <Spacer height={10} />
+            <TextArea
+              name="cancle"
+              placeholder="상세 사유를 입력해주세요.(최대 300자)"
+              width="100%"
+              height="162px"
+              padding="13px"
+              maxLength={300}
+              ref={reasonRef}
+            />
+          </InputItemContainer>
           <Spacer height={10} />
-          <SelectDropDown
-            menuItems={['단순변심', '배송지연', '주문실수', '서비스 불만족']}
-            setTitle={setReasonTitle}
-            selectTitle="취소 사유 선택"
-          />
-          <Spacer height={12} />
-          <Text $fontType="H3" color="white">
-            상세 사유 &#40;선택&#41;
-          </Text>
-          <Spacer height={10} />
-          <TextArea
-            name="cancle"
-            placeholder="상세 사유를 입력해주세요.(최대 300자)"
-            width="100%"
-            height="162px"
-            padding="13px"
-            value={reason}
-            onChange={(e) => handleInputChange(e)}
-            maxLength={300}
-            ref={reasonRef}
-          />
-        </InputItemContainer>
-        <Spacer height={10} />
-        <InputStyle name="cancle" placeholder="-취소과 관련된 안내사항이 들어갑니다." width="100%" readOnly={true} />
-        <Spacer height={89} />
-        <CommonButton width={'100%'} type={ButtonType.Secondary} onClick={() => handleSubmit()}>
-          확인
-        </CommonButton>
+          <TextBox width="100%">
+            <Text $fontType="Body04" color="grey30">
+              - 취소관련 안내사항
+            </Text>
+          </TextBox>
+          <Spacer height={89} />
+          <CommonButton width={'100%'} type={ButtonType.Secondary} htmlType="submit">
+            확인
+          </CommonButton>
+        </form>
       </Modal>
     </>
   );
