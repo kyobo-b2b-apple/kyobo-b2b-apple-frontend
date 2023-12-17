765f084c28353e8106d1fbb73b3181e6ee172bcb
hyeonji <55472485+hyeonzii@users.noreply.github.com>
2023-12-17 18:38:11 +0900

    [개발] 주문 취소 상세 조회 (#23)
    - 주문 취소 상세 조회 관련 API 코드 추가

--- src/api/cancleModalApi.ts
+++ src/api/cancleModalApi.ts
@@ -9,3 +9,8 @@ export const cancleProductAPi = async (cancleProps: CancleProps, orderId: number
   console.log('api호출: ' + cancleProps);
   return axiosInstance.post(`/api/orders/${orderId}/cancel`, { cancleProps });
 };
+
+export const getCancleDetailAPi = async (cancelCode: string) => {
+  console.log('getCancleDetailAPi');
+  return axiosInstance.get(`/api/orders/${cancelCode}/cancel`, {});
+};
